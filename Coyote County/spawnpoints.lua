783e24a95ccea8c90857e24a8021e9a50b7c4c12
SageAzakaela <sageazakaela@gmail.com>
2022-09-22 15:52:35 -0600

    init
    initial commit -- need to figure out where to store the buildings

---
+++ Coyote County/spawnpoints.lua
@@ -0,0 +1,22 @@
+function SpawnPoints()
+return {
+  constructionworker = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  },
+  fireofficer = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  },
+  parkranger = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  },
+  policeofficer = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  },
+  securityguard = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  },
+  unemployed = {
+    { worldX = 2, worldY = 12, posX = 154, posY = 242, posZ = 2 }
+  }
+}
+end
